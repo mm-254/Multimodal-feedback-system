99483095ae1acc07cb402c1b9d4fd4d1cef7ce79
mm_254 <60447937+mm-254@users.noreply.github.com>
2021-02-18 14:16:03 -0500

    Update tempModule.ino

--- tempModule.ino
+++ tempModule.ino
@@ -1,7 +1,7 @@
 #include <PID_v1.h>
 #include <Wire.h>
 
-float incomingFloat;
+int incomingInt;
 float val =0.0;
 int cooler = 3;
 int heater = 5;
@@ -16,14 +16,18 @@ double prev_val;
 PID tempPID(&currentTemp, &PID_value, &desiredTemp,2.6,2,0.5, DIRECT); //constants in order kp, ki, kd
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+ // Serial.setTimeout(300);
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
   pinMode(heater, OUTPUT);
   pinMode(cooler, OUTPUT);
+  analogReference(EXTERNAL);
   //Time = millis(); 
 
   //Initialize variables
   currentTemp= analogRead(tempSensor);
-  currentTemp= ((5*currentTemp/1023)-0.5)*100; //celsius
+  currentTemp= ((3.3*currentTemp/1023)-0.5)*100; //celsius
   currentTemp = (currentTemp * 9.0 / 5.0) + 32.0 ;//fahrenheit
   desiredTemp= currentTemp+1;
 
@@ -32,36 +36,42 @@ void setup() {
   
 }
 
-void loop() {
+void loop() {  
 
   //Read heater and cooler element temperatures
   currentTemp= analogRead(tempSensor);
-  currentTemp= ((5*currentTemp/1023)-0.5)*100; //celsius
+  currentTemp= ((3.3*currentTemp/1024)-0.5)*100; //celsius
   currentTemp = (currentTemp * 9.0 / 5.0) + 32.0 ;//fahrenheit
-  Serial.print("current:");
-  Serial.println(currentTemp);
+ // Serial.print("current:");
+  //Serial.println(currentTemp);
   
-   //Safety Thressholds
+   //Safety Thresholds
    if(currentTemp>=140 || currentTemp<=60){
     analogWrite(heater, 0);
     return;  
    }
-   
+    Serial.read();
+    delay(200);
   //Read incoming temperature reading
   if (Serial.available() > 0) {
        
     //incomingByte = Serial.readString();
-    incomingFloat = Serial.parseFloat();
+    incomingInt = Serial.parseInt();
+    
     // say what you got:
+    //Serial.print("desired:");
+    //Serial.println(incomingInt);
 
-    if(incomingFloat!=0){
-          desiredTemp = incomingFloat;
-          Serial.flush();
-          Serial.print("desired:");
-          Serial.println(desiredTemp);
-    }
-    }
+    if(incomingInt>60){ //thresholding temperatures at 60F
+          desiredTemp = incomingInt;
+          digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+          delay(10);                       // wait for a second
+          digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW 
+          //Serial.print("desired:");
+          //Serial.println(desiredTemp);
+     }
 
+  }
       // PID:
     if(desiredTemp!=0){
        
@@ -71,24 +81,24 @@ void loop() {
           tempPID.Compute();
           //write the PWM signal to the mosfet on digital pin
           analogWrite(heater,PID_value);
-          Serial.print("HOutput: ");
-          Serial.println(PID_value);
+          //Serial.print("HOutput: ");
+          //Serial.println(PID_value);
        }
        if (desiredTemp<currentTemp && currentTemp>40){
           tempPID.SetControllerDirection(REVERSE);  //Reverse direction of controller (increase cooling to decrease temperature)
           tempPID.Compute();
           //write the PWM signal to the mosfet on digital pin
           analogWrite(cooler,PID_value);
-          Serial.print("COutput: "); 
-          Serial.println(PID_value);
+          //Serial.print("COutput: "); 
+          //Serial.println(PID_value);
        } 
 
-       delay(300);
+       delay(100);
 
     }else{
       analogWrite(heater, 0);
       analogWrite(cooler, 0);
   }
-            
-delay(500);
+
+
 }
