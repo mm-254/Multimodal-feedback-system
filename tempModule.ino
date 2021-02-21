d997b58121ce6be806736de035182fcb0f0fe0fd
mm_254 <60447937+mm-254@users.noreply.github.com>
2021-02-20 22:42:12 -0500

    Update tempModule.ino

--- tempModule.ino
+++ tempModule.ino
@@ -1,7 +1,9 @@
 #include <PID_v1.h>
 #include <Wire.h>
+#include <FIR.h>
 
 int incomingInt;
+float incomingFloat;
 float val =0.0;
 int cooler = 3;
 int heater = 5;
@@ -9,11 +11,18 @@ int tempSensor = A0;
 double currentTemp;
 double desiredTemp;
 double PID_value = 0;
+double PID_value2 = 0;
 double prev_val;
 
 
 //Specify the links and initial tuning parameters
-PID tempPID(&currentTemp, &PID_value, &desiredTemp,2.6,2,0.5, DIRECT); //constants in order kp, ki, kd
+//Heater PID
+PID heatPID(&currentTemp, &PID_value, &desiredTemp,12.5,2.5,0.65, DIRECT); //constants in order kp, ki, kd
+//Cooler PID
+PID coolPID(&currentTemp, &PID_value2, &desiredTemp,12.75,3.85,0.9, REVERSE); //constants in order kp, ki, kd
+
+//Moving average filter, five samples
+FIR<float, 5> fir_avg;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -32,65 +41,87 @@ void setup() {
   desiredTemp= currentTemp+1;
 
   //turn the PID on
-  tempPID.SetMode(AUTOMATIC);
-  
+  heatPID.SetMode(AUTOMATIC);
+  coolPID.SetMode(AUTOMATIC);
+
+  //filter coefficients
+  float coef_avg[5] = {1., 1., 1., 1., 1.};
+
+  // Set the coefficients
+  fir_avg.setFilterCoeffs(coef_avg);
+
+  // Set the gain
+ // Serial.print("Moving Average Filter Gain: ");
+ // Serial.println(fir_avg.getGain());
 }
 
-void loop() {  
+void loop() {
 
   //Read heater and cooler element temperatures
   currentTemp= analogRead(tempSensor);
-  currentTemp= ((3.3*currentTemp/1024)-0.5)*100; //celsius
+  currentTemp= ((3.3*currentTemp/1023)-0.5)*100; //celsius
   currentTemp = (currentTemp * 9.0 / 5.0) + 32.0 ;//fahrenheit
- // Serial.print("current:");
-  //Serial.println(currentTemp);
   
-   //Safety Thresholds
-   if(currentTemp>=140 || currentTemp<=60){
+   //Safety Thressholds
+   if(currentTemp>=140 || currentTemp<60){
     analogWrite(heater, 0);
     return;  
    }
-    Serial.read();
-    delay(200);
+   //Serial.read();
+   //delay(200);
+   
+   currentTemp = fir_avg.processReading(currentTemp);
+   //Serial.print("current:");
+   Serial.println(currentTemp);
+   
   //Read incoming temperature reading
   if (Serial.available() > 0) {
        
     //incomingByte = Serial.readString();
     incomingInt = Serial.parseInt();
+    //incomingFloat = Serial.parseFloat();
     
     // say what you got:
-    //Serial.print("desired:");
-    //Serial.println(incomingInt);
 
-    if(incomingInt>60){ //thresholding temperatures at 60F
+    if(incomingInt>=50){ //thresholding temperatures at 60F
           desiredTemp = incomingInt;
           digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
           delay(10);                       // wait for a second
           digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW 
           //Serial.print("desired:");
-          //Serial.println(desiredTemp);
+          Serial.println(desiredTemp);
      }
+    }
 
-  }
       // PID:
-    if(desiredTemp!=0){
+    if(desiredTemp>=50){
        
         //Adjust heater/cooler
        if (desiredTemp>currentTemp && currentTemp<150){
-          tempPID.SetControllerDirection(DIRECT); //Reset direction if reversed
-          tempPID.Compute();
+          //heatPID.SetControllerDirection(DIRECT); //As temp increases heat output can decrease?
+          heatPID.Compute();
           //write the PWM signal to the mosfet on digital pin
-          analogWrite(heater,PID_value);
+          analogWrite(cooler, 0);
+          if(PID_value>255){
+            analogWrite(heater, 255);
+          }else{
+            analogWrite(heater,PID_value);
+          }
           //Serial.print("HOutput: ");
           //Serial.println(PID_value);
        }
        if (desiredTemp<currentTemp && currentTemp>40){
-          tempPID.SetControllerDirection(REVERSE);  //Reverse direction of controller (increase cooling to decrease temperature)
-          tempPID.Compute();
+          //heatPID.SetControllerDirection(DIRECT); //As temp increases cold output should increase
+          coolPID.Compute();
           //write the PWM signal to the mosfet on digital pin
-          analogWrite(cooler,PID_value);
-          //Serial.print("COutput: "); 
-          //Serial.println(PID_value);
+          analogWrite(heater, 0);
+          if(PID_value2>255){
+            analogWrite(cooler, 255);
+          }else{
+            analogWrite(cooler,PID_value2);
+          }
+         //Serial.print("COutput: "); 
+         //Serial.println(PID_value2);
        } 
 
        delay(100);
@@ -99,6 +130,6 @@ void loop() {
       analogWrite(heater, 0);
       analogWrite(cooler, 0);
   }
-
-
+            
+delay(500);
 }
